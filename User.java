03311161028b98b877f59547426da02b95478c4a
Ryan Zhou <110564801+RyanZCode@users.noreply.github.com>
2023-01-11 15:19:39 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,7 +1,18 @@
 package gritnessApp;
 
+import java.util.*;
+
 public class User {
-	String name;
+	String displayName;
 	String username;
+	double weight;
+	String goal;
+	ArrayList<Workout> userWorkoutTemplates;
+	ArrayList<Workout> userMealTemplates;
+	
+	public User(String username, String displayName) {
+		this.username = username;
+		this.displayName = displayName;
+	}
 	
 }
