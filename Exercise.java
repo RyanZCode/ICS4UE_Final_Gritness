607d58623bcdbcc097e6ba4f53586f6579bac709
carrizo93 <nathankong93@gmail.com>
2023-01-14 15:14:31 -0500

    Merge branch 'main' of https://github.com/RyanZCode/ICS4UE_Final_Gritness

---
+++ Exercise.java
@@ -0,0 +1,19 @@
+package gritnessApp;
+
+import java.util.*;
+
+public class Exercise {
+	String name;
+	ArrayList<Set> sets;
+	
+}
+
+class Set {
+	double lbs;
+	int reps;
+	
+	Set(double lbs, int reps) {
+		this.lbs = lbs;
+		this.reps = reps;
+	}
+}
--- User.java
+++ User.java
@@ -5,14 +5,22 @@ import java.util.*;
 public class User {
 	String displayName;
 	String username;
+	String password;
 	double weight;
 	String goal;
 	ArrayList<Workout> userWorkoutTemplates;
 	ArrayList<Workout> userMealTemplates;
 	
-	public User(String username, String displayName) {
+	public User(String username, String displayName, String password) {
 		this.username = username;
 		this.displayName = displayName;
+		this.password = password;
 	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	
 	
 }
