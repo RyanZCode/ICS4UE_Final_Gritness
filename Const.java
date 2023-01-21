25af8db5be9ffb10c6fa7dc71733adcedc1be9ed
justinzhoucode <114521027+justinzhoucode@users.noreply.github.com>
2023-01-21 12:07:37 -0500

    Add files via upload

--- Const.java
+++ Const.java
@@ -1,8 +1,21 @@
 package gritnessApp;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+
 public class Const {
     public static final int MAIN_LENGTH = 1280;
     public static final int MAIN_WIDTH = 720;
     public static final int LOGIN_LENGTH = 500;
     public static final int LOGIN_WIDTH = 500;
+    public static final Color NAV_BAR_COLOUR = new Color(80, 120, 160);
+    public static final ImageIcon PROFILE_ICON = new ImageIcon("person.png");
+    public static final ImageIcon HISTORY_ICON = new ImageIcon("clock.png");
+    public static final ImageIcon WORKOUT_ICON = new ImageIcon("barbell.png");
+    public static final ImageIcon FOOD_ICON = new ImageIcon("food.png");
+    public static final ImageIcon SOCIAL_ICON = new ImageIcon("social.png");
+    public static final Font BUTTON_FONT = new Font("Calibri", Font.PLAIN, 35);
+    public static final Font TEXT_FONT = new Font("Calibri", Font.PLAIN, 25);
 }
