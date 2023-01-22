bb2702e73479406a62f4c11eb37c95ce7c80c650
Jason Wu <66051354+Jason10293@users.noreply.github.com>
2023-01-22 15:13:29 -0500

    Add files via upload

--- BarGraph.java
+++ BarGraph.java
@@ -2,31 +2,14 @@ package gritnessApp;
 import java.awt.*;
 import javax.swing.*;
 
-public class BarGraph extends JPanel {
-    private int[] data;
-    private String xAxisLabel;
-    private String yAxisLabel;
-    private String title;
-    private int x, y;
-    final int AXIS_LENGTH = 250;
-    final int LABEL_DISTANCE_BETWEEN_AXIS = 35;
-    final int MAX_VALUE = 5;
-    final int NUMBER_DAYS = 7;
-    final String[] DAYS_OF_WEEK = {"S","M","T","W","T","F","S"};
-    
-    
-    public BarGraph(int[] data, String xAxisLabel, String yAxisLabel, String title, int x, int y) {
-        this.data = data;
-        this.xAxisLabel = xAxisLabel;
-        this.yAxisLabel = yAxisLabel;
-        this.title = title;
-        this.x = x;
-        this.y = y;
+public class BarGraph extends Graph{    
+    BarGraph(int[] data, String xAxisLabel, String yAxisLabel, String title, int x, int y, int maxValue) {
+    	super(data, xAxisLabel, yAxisLabel, title, x, y, maxValue);
     }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    
+    
+    public void draw(Graphics g) {
+    	
 
         // Draw x and y axis
         
@@ -62,15 +45,5 @@ public class BarGraph extends JPanel {
         for(int i = 0; i < 7; i++) {
         	g.drawString(DAYS_OF_WEEK[i], x + i * AXIS_LENGTH / 7, y + 15);
         }
-    }
-
-    public static void main(String[] args) {
-        int[] data = {1, 2, 3, 4, 3, 2, 3};
-
-        JFrame frame = new JFrame();
-        frame.setSize(600, 500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new BarGraph(data, "Day", "Workouts", "Workouts", 150, 350));
-        frame.setVisible(true);
     }
 }
--- LineGraph.java
+++ LineGraph.java
@@ -2,31 +2,12 @@ package gritnessApp;
 import java.awt.*;
 import javax.swing.*;
 
-public class LineGraph extends JPanel {
-    private int[] data;
-    private String xAxisLabel;
-    private String yAxisLabel;
-    private String title;
-    private int x, y;
-    final int AXIS_LENGTH = 250;
-    final int LABEL_DISTANCE_BETWEEN_AXIS = 35;
-    final int MAX_VALUE = 2500;
-    final int NUMBER_DAYS = 7;
-    final String[] DAYS_OF_WEEK = {"S","M","T","W","T","F","S"};
-    
-    
-    public LineGraph(int[] data, String xAxisLabel, String yAxisLabel, String title, int x, int y) {
-        this.data = data;
-        this.xAxisLabel = xAxisLabel;
-        this.yAxisLabel = yAxisLabel;
-        this.title = title;
-        this.x = x;
-        this.y = y;
+public class LineGraph extends Graph{
+    public LineGraph(int[] data, String xAxisLabel, String yAxisLabel, String title, int x, int y, int maxValue) {
+    	super(data, xAxisLabel, yAxisLabel, title, x, y, maxValue);
     }
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    public void draw(Graphics g) {
 
         // Draw x and y axis
         // x= 50, y = 250
@@ -65,15 +46,5 @@ public class LineGraph extends JPanel {
         for(int i = 0; i < 7; i++) {
         	g.drawString(DAYS_OF_WEEK[i], x + i * AXIS_LENGTH / 7, y + 15);
         }
-    }
-
-    public static void main(String[] args) {
-        int[] data = {450, 300, 1500, 1770, 690, 580, 280};
-
-        JFrame frame = new JFrame();
-        frame.setSize(600, 500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new LineGraph(data, "Day", "Calories", "Calories", 150, 350));
-        frame.setVisible(true);
     }
 }
