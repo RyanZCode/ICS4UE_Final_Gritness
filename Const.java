a5a9862e10afe4a9b999a1c1374c7da5f44939aa
Jason Wu <66051354+Jason10293@users.noreply.github.com>
2023-01-23 16:50:31 -0500

    Add files via upload

---
+++ Const.java
@@ -0,0 +1,27 @@
+package gritnessApp;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+
+public class Const {
+    public static final int MAIN_LENGTH = 1280;
+    public static final int MAIN_WIDTH = 720;
+    public static final int LOGIN_LENGTH = 500;
+    public static final int LOGIN_WIDTH = 500;
+    public static final Color BACKGROUND_COLOUR = new Color(238,238,238);
+    public static final Color NAV_BAR_COLOUR = new Color(80, 120, 160);
+    public static final Font PROFILE_BUTTON_FONT = new Font("Calibri", Font.PLAIN, 45);
+    public static final ImageIcon PROFILE_ICON = new ImageIcon("src/gritnessApp.client.icons/person.png");
+    public static final ImageIcon HISTORY_ICON = new ImageIcon("src/gritnessApp.client.icons/clock.png");
+    public static final ImageIcon WORKOUT_ICON = new ImageIcon("src/gritnessApp.client.icons/barbell.png");
+    public static final ImageIcon FOOD_ICON = new ImageIcon("src/gritnessApp.client.icons/food.png");
+    public static final ImageIcon SOCIAL_ICON = new ImageIcon("src/gritnessApp.client.icons/social.png");
+    public static final Font BUTTON_FONT = new Font("Calibri", Font.PLAIN, 35);
+    public static final Font TEXT_FONT = new Font("Calibri", Font.PLAIN, 25);
+    public static final Font TITLE_FONT = new Font("Calibri", Font.BOLD, 60);
+    public static final int NAV_Y = 570;
+    public static final int NAV_WIDTH = 125;
+    public static final int NAV_LENGTH = 270;
+}
