447906cb2744407d6d249880a5200434a18eef38
justinzhoucode <114521027+justinzhoucode@users.noreply.github.com>
2023-01-23 18:45:31 -0500

    Add files via upload

--- client/Const.java
+++ client/Const.java
@@ -11,15 +11,18 @@ public class Const {
     public static final int LOGIN_LENGTH = 500;
     public static final int LOGIN_WIDTH = 500;
     public static final Color NAV_BAR_COLOUR = new Color(80, 120, 160);
-    public static final ImageIcon PROFILE_ICON = new ImageIcon("src/gritnessApp.client.icons/person.png");
-    public static final ImageIcon HISTORY_ICON = new ImageIcon("src/gritnessApp.client.icons/clock.png");
-    public static final ImageIcon WORKOUT_ICON = new ImageIcon("src/gritnessApp.client.icons/barbell.png");
-    public static final ImageIcon FOOD_ICON = new ImageIcon("src/gritnessApp.client.icons/food.png");
-    public static final ImageIcon SOCIAL_ICON = new ImageIcon("src/gritnessApp.client.icons/social.png");
+    public static final Color BUTTON_COLOUR = new Color(100, 140, 180);
+    public static final Color BUTTON_COLOUR2 = new Color(98, 121, 144);
+    public static final Color BACKGROUND_COLOUR = new Color(255, 255, 255);
+    public static final ImageIcon PROFILE_ICON = new ImageIcon("person.png");
+    public static final ImageIcon HISTORY_ICON = new ImageIcon("clock.png");
+    public static final ImageIcon WORKOUT_ICON = new ImageIcon("barbell.png");
+    public static final ImageIcon FOOD_ICON = new ImageIcon("food.png");
+    public static final ImageIcon SOCIAL_ICON = new ImageIcon("social.png");
     public static final Font BUTTON_FONT = new Font("Calibri", Font.PLAIN, 35);
+    public static final Font BUTTON_FONT2 = new Font("Calibri", Font.PLAIN, 25);
     public static final Font TEXT_FONT = new Font("Calibri", Font.PLAIN, 25);
-    public static final Font TITLE_FONT = new Font("Calibri", Font.BOLD, 60);
-    public static final int NAV_Y = 570;
-    public static final int NAV_WIDTH = 125;
-    public static final int NAV_LENGTH = 270;
+    public static final Font SUBTITLE_FONT = new Font("Calibri", Font.PLAIN, 30);
+    public static final Font TITLE_FONT = new Font("Calibri", Font.BOLD, 45);
+    public static final ImageIcon PROFILE_PIC = new ImageIcon("profilepic.png");
 }
