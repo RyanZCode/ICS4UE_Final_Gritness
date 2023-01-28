84efb0d30838e188df6fcf9feb8fb63426acf838
carrizo93 <nathankong93@gmail.com>
2023-01-27 22:00:08 -0500

    Update NutritionTab.java

--- NutritionTab.java
+++ NutritionTab.java
@@ -1,97 +1,133 @@
-package gritnessApp;
+package gritnessApp.client;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class NutritionTab extends JPanel implements ActionListener{
-   JLabel age;
-   JButton profile, workout, food, social, history;
+    JLabel meal, calories, protein, carbs, sugar, fiber, fats, sodium;
+    JTextField mealNameField, caloriesField, proteinField, carbsField, sugarField, fiberField, fatsField, sodiumField;
+    JButton profile, workout, food, social, history, addMeal;
+    JTable table;
+    JScrollPane scroll;
+    String[] columnNames = {"Meal","Calories", "Protein", "Carbs", "Sugar", "Fiber", "Fats", "Sodium"};
+    Object[] row = new Object[8];
+
     NutritionTab(){
-        age = new JLabel("Nutrition");
-        age.setFont(Const.TEXT_FONT);
-        age.setBounds(50, 50, 500, 500);
-        this.add(age);
-        
-        profile = new JButton ("Profile");
-        profile.setBackground(Const.NAV_BAR_COLOUR);
-        profile.setForeground(Color.white);
-        profile.setFocusable(false);
-        profile.addActionListener(this);
-        profile.setBorderPainted(false);
-        profile.setIcon(Const.PROFILE_ICON);
-        profile.setFont(Const.BUTTON_FONT);
-        profile.setBounds(0, 570, 270, 125);
-        profile.setHorizontalTextPosition(JButton.CENTER);
-        profile.setVerticalTextPosition(JButton.BOTTOM);
+        //        age = new JLabel("Nutrition");
+        //        age.setFont(Const.TEXT_FONT);
+        //        age.setBounds(50, 50, 500, 500);
+        //        this.add(age)
+        //meals[0] =  {"Chicken Shawarma", "500", "20", "30", "5", "5", "15", "300"};
+        meal = newText("Meal:", 280);
+        calories = newText("Calories:", 310);
+        protein = newText("Protein:", 340);
+        carbs = newText("Carbs:", 370);
+        sugar = newText("Sugar:", 400);
+        fiber = newText("Fiber:", 430);
+        fats = newText("Fats:", 460);
+        sodium = newText("Sodium:", 490);
 
-        history = new JButton ("History");
-        history.setBackground(Const.NAV_BAR_COLOUR);
-        history.setForeground(Color.white);
-        history.setFocusable(false);
-        history.setBorderPainted(false);
-        history.addActionListener(this);
-        history.setIcon(Const.HISTORY_ICON);
-        history.setFont(Const.BUTTON_FONT);
-        history.setBounds(250, 570, 270, 125);
-        history.setHorizontalTextPosition(JButton.CENTER);
-        history.setVerticalTextPosition(JButton.BOTTOM);
+        mealNameField = newField(280);
+        caloriesField = newField(310);
+        proteinField = newField(340);
+        carbsField = newField(370);
+        sugarField = newField(400);
+        fiberField = newField(430);
+        fatsField = newField(460);
+        sodiumField = newField(490);
 
-        workout = new JButton ("Workout");
-        workout.setBackground(Const.NAV_BAR_COLOUR);
-        workout.setForeground(Color.white);
-        workout.setFocusable(false);
-        workout.setBorderPainted(false);
-        workout.addActionListener(this);
-        workout.setIcon(Const.WORKOUT_ICON);
-        workout.setFont(Const.BUTTON_FONT);
-        workout.setBounds(500, 570, 270, 125);
-        workout.setHorizontalTextPosition(JButton.CENTER);
-        workout.setVerticalTextPosition(JButton.BOTTOM);
+        DefaultTableModel model = new DefaultTableModel();
+        table = new JTable(model);
+        model.setColumnIdentifiers(columnNames);
 
-        food = new JButton ("Food");
-        food.setBackground(Const.NAV_BAR_COLOUR);
-        food.setForeground(Color.white);
-        food.setFocusable(false);
-        food.setBorderPainted(false);
-        food.addActionListener(this);
-        food.setIcon(Const.FOOD_ICON);
-        food.setFont(Const.BUTTON_FONT);
-        food.setBounds(750, 570, 270, 125);
-        food.setHorizontalTextPosition(JButton.CENTER);
-        food.setVerticalTextPosition(JButton.BOTTOM);
+        scroll = new JScrollPane(table);
+        scroll.setBounds(100, 270, 800, 300);
+        this.add(scroll);  
 
-        social = new JButton ("Social");
-        social.setBackground(Const.NAV_BAR_COLOUR);
-        social.setForeground(Color.white);
-        social.setFocusable(false);
-        social.setBorderPainted(false);
-        social.addActionListener(this);
-        social.setIcon(Const.SOCIAL_ICON);
-        social.setFont(Const.BUTTON_FONT);
-        social.setBounds(1000, 570, 270, 125);
-        social.setHorizontalTextPosition(JButton.CENTER);
-        social.setVerticalTextPosition(JButton.BOTTOM);
-        
+        addMeal = new JButton("Add meal");
+        addMeal.setBounds(1010, 520, 129, 20);
+        this.add(addMeal);
+
+        profile =  newNavBarButton ("Profile", 0, Const.PROFILE_ICON);
+        history =  newNavBarButton ("History", 250, Const.HISTORY_ICON);
+        workout =  newNavBarButton ("Workout", 500, Const.WORKOUT_ICON);
+        food =  newNavBarButton ("Food", 750, Const.FOOD_ICON);
+        social =  newNavBarButton ("Social", 1000, Const.SOCIAL_ICON);
+
         profile.addActionListener(this);
         history.addActionListener(this);
         workout.addActionListener(this);
         food.addActionListener(this);
         social.addActionListener(this);
-        
+
         this.add(profile);
         this.add(history);
         this.add(workout);
         this.add(food);
         this.add(social);
-        
         this.setLayout(null);
+
+        addMeal.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                row[0] = mealNameField.getText();
+                row[1] = caloriesField.getText();
+                row[2] = proteinField.getText();
+                row[3] = carbsField.getText();
+                row[4] = sugarField.getText();
+                row[5] = fiberField.getText();
+                row[6] = fatsField.getText();
+                row[7] = sodiumField.getText();
+                model.addRow(row);
+            }
+        });   
     }
-    @Override
+
+
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+    }
+
+    public JLabel newText(String text, int y) {
+        JLabel label = new JLabel(text);
+        label.setBounds(975, y, 120, 20);
+        label.setFont(Const.SMALLER_FONT);
+        this.add(label);
+        return label;
+    }
+
+    public JTextField newField(int y){
+        JTextField button = new JTextField();
+        button.setBounds(1075, y, 120, 20);
+        this.add(button);
+        return button;
+    }
+
+    public JButton newNavBarButton(String name, int x, ImageIcon icon) {
+        JButton button = new JButton(name);
+        button.setBackground(Const.NAV_BAR_COLOUR);
+        button.setForeground(Color.white);
+        button.setFocusable(false);
+        button.setBorderPainted(false);
+        button.addActionListener(this);
+        button.setIcon(icon);
+        button.setFont(Const.BUTTON_FONT);
+        button.setBounds(x, 570, 270, 125);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        this.add(button);
+        return button;
+    }
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         if(e.getSource() == profile) {
@@ -113,6 +149,7 @@ public class NutritionTab extends JPanel implements ActionListener{
         else if (e.getSource() == social) {
             Window.layout.show(Window.container, "social");
         }
+               
     }
 
 }
