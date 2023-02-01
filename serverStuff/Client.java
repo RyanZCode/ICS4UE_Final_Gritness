1d83f0b7df275c8f9f2732122910c2cda05d1552
justinzhoucode <114521027+justinzhoucode@users.noreply.github.com>
2023-01-31 21:25:47 -0500

    Add files via upload

--- serverStuff/Client.java
+++ serverStuff/Client.java
@@ -1,6 +1,5 @@
 package gritnessApp;
 
-import java.awt.Dimension;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,12 +7,16 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Random;
 
-import javax.swing.JFrame;
-
+/**
+ * [Client.java]
+ * This class obtains information and data from server
+ * @author Ryan Zhou
+ * @author Jason Wu
+ * @version 1.0 Jan 24, 2023
+ */
 public class Client implements Runnable {
-	final String LOCAL_HOST = "localhost";
+	final String LOCAL_HOST = "10.0.0.7";
 	final int PORT = 10111;
 	Socket clientSocket;
 	PrintWriter output;
@@ -22,9 +25,18 @@ public class Client implements Runnable {
 	Queue<String> dataQ = new LinkedList<>();
 	private String username;
 
+	/**
+	 * Client
+	 * Client constructor
+	 */
 	Client() {
 	}
 
+	/**
+	 * start
+	 * Connects to the server
+	 * @throws Exception
+	 */
 	public void start() throws Exception {
 		// create a socket with the local IP address and attempt a connection
 		System.out.println("Connecting to server...");
@@ -33,13 +45,19 @@ public class Client implements Runnable {
 		clientSocket = new Socket(LOCAL_HOST, PORT);
 		output = new PrintWriter(clientSocket.getOutputStream());
 		input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		System.out.println("Connected succesfully!");
+		System.out.println("Connected successfully!");
 
 		try {Thread.sleep(5);} catch (InterruptedException e) {throw new RuntimeException(e);}
 	}
 
-	// maybe make the identifiers "login", "signup", etc. into constants
-	// Send login data to server, receive whether login was successful or not
+	/**
+	 * sendLogin
+	 * Sends the login data to the server and receives whether successful or not
+	 * @param username User's name
+	 * @param password User's password
+	 * @return successful or unsuccessful login
+	 * @throws IOException
+	 */
 	public String sendLogin(String username, String password) throws IOException {
 		// Send message to server
 		output.println("login" + "$$" + username + "$$" + password);
@@ -54,7 +72,14 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 
-	// Send signup data to server, receive whether signup was successful or not
+	/**
+	 * sendSignUp
+	 * Sends signup data to server and receives whether signup was successful or not
+	 * @param username User's username
+	 * @param password User's password
+	 * @return successful or unsuccessful signup
+	 * @throws IOException
+	 */
 	public String sendSignUp(String username, String password) throws IOException {
 		// Send message to server
 		output.println("signup" + "$$" + username + "$$" + password);
@@ -70,42 +95,68 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Sends user age to server
+	/**
+	 * sendAge
+	 * Sends user's age to server
+	 * @param age User's age
+	 */
 	public void sendAge(int age) {
 		// Send message to server
 		output.println("sendAge" + "$$" + username + "$$" + age);
 		output.flush();
 	}
 	
-	// Sends user weight to server
+
+	/**
+	 * sendWeight
+	 * Sends user's weight to server
+	 * @param weight User's weight
+	 */
 	public void sendWeight(double weight) {
 		// Send message to server
 		output.println("sendWeight" + "$$" + username + "$$" + weight);
 		output.flush();
 	}
 	
-	// Sends user height to server
+	/**
+	 * sendHeight
+	 * Sends user's height to server
+	 * @param height User's Height
+	 */
 	public void sendHeight(int height) {
 		// Send message to server
 		output.println("sendHeight" + "$$" + username + "$$" + height);
 		output.flush();
 	}
 	
-	// Sends user display name to server
+	/**
+	 * sendName
+	 * Sends user's display name to server
+	 * @param name User's display name
+	 */
 	public void sendName(String name) {
 		// Send message to server
 		output.println("sendName" + "$$" + username + "$$" + name);
 		output.flush();
 	}
 	
-	// Sends user calorie goal to server
+	/**
+	 * sendCalorieGoal
+	 * Sends user's calorie goal to server
+	 * @param calorieGoal User's calorie goal
+	 */
 	public void sendCalorieGoal(int calorieGoal) {
 		// Send message to server
 		output.println("sendCalorieGoal" + "$$" + username + "$$" + calorieGoal);
 		output.flush();
 	}
 	
-	// Get profile data from server
+	/**
+	 * getProfileInfo
+	 * Gets profile data from server
+	 * @return sent profile data
+	 * @throws IOException
+	 */
 	public String getProfileInfo() throws IOException {
 		// Send message to server
 		output.println("getProfileInfo" + "$$" + username);
@@ -121,7 +172,13 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get profile data from server
+	/**
+	 * getProfileCalHistory
+	 * Gets user's calorie history from server
+	 * @param username User's display name
+	 * @return sent calorie history
+	 * @throws IOException
+	 */
 	public String getProfileCalHistory(String username) throws IOException {
 		// Send message to server
 		output.println("getProfileCalHistory" + "$$" + username);
@@ -137,7 +194,14 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get profile data from server
+	
+	/**
+	 * getProfileWorkoutNumHistory
+	 * Gets user's workout history from server
+	 * @param username User's display name
+	 * @return sent workout history
+	 * @throws IOException
+	 */
 	public String getProfileWorkoutNumHistory(String username) throws IOException {
 		// Send message to server
 		output.println("getProfileWorkoutNumHistory" + "$$" + username);
@@ -153,12 +217,21 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get username
+	/**
+	 * getUsername
+	 * Get user's username
+	 * @return username
+	 */
 	public String getUsername() {
 		return this.username;
 	}
 	
-	// Get display name from server
+	/**
+	 * getDisplayName
+	 * Gets user's display name from server
+	 * @return sent display name
+	 * @throws IOException
+	 */
 	public String getDisplayName() throws IOException {
 		// Send message to server
 		output.println("getDisplayName" + "$$" + username);
@@ -174,7 +247,13 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get friend's display name from server
+	/**
+	 * getFriendData
+	 * Gets friends data from server
+	 * @param friendUsername Friend's display name
+	 * @return sent friends data name
+	 * @throws IOException
+	 */
 	public String getFriendData(String friendUsername) throws IOException {
 		// Send message to server
 		output.println("getFriendData" + "$$" + friendUsername);
@@ -190,7 +269,12 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get friend usernames from server
+	/**
+	 * getFriendUsernames
+	 * Gets friends display name from server
+	 * @return sent friends data name
+	 * @throws IOException
+	 */
 	public String getFriendUsernames() throws IOException {
 		// Send message to server
 		output.println("getFriendUsernames" + "$$" + username);
@@ -206,7 +290,13 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Send a friend request to server
+	/**
+	 * sendFriendRequest
+	 * Sends friend request to server
+	 * @param friendUsername Friend's display name
+	 * @return request
+	 * @throws IOException
+	 */
 	public String sendFriendRequest(String friendUsername) throws IOException {
 		// Send message to server
 		output.println("sendFriendRequest" + "$$" + username + "$$" + friendUsername);
@@ -222,7 +312,14 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Send workout data to server
+	/**
+	 * sendWorkoutData
+	 * Sends workout data to the server
+	 * @param workoutName Name of workout
+	 * @param elapsedTime Time taken for workout
+	 * @return sent workout data
+	 * @throws IOException
+	 */
 	public String sendWorkoutData(String workoutName, int elapsedTime) throws IOException {
 		// Send message to server
 		output.println("sendWorkoutData" + "$$" + username + "$$" + workoutName + "$$" + elapsedTime);
@@ -238,7 +335,12 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get history data from server
+	/**
+	 * getHistory
+	 * Gets history from server
+	 * @return history
+	 * @throws IOException
+	 */
 	public String getHistory() throws IOException {
 		// Send message to server
 		output.println("historyTab" + "$$" + username);
@@ -254,7 +356,12 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get workout data from server
+	/**
+	 * getWorkouts
+	 * Gets workout data from server
+	 * @return workout data
+	 * @throws IOException
+	 */
 	public String getWorkouts() throws IOException {
 		// Send message to server
 		output.println("workoutTab" + "$$" + username);
@@ -270,7 +377,12 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get food data from server
+	/**
+	 * getFood
+	 * Gets food data from server
+	 * @return food data
+	 * @throws IOException
+	 */
 	public String getFood() throws IOException {
 		// Send message to server
 		output.println("foodTab" + "$$" + username);
@@ -286,7 +398,12 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get social data from server
+	/**
+	 * getSocial
+	 * Gets social data from server
+	 * @return social data
+	 * @throws IOException
+	 */
 	public String getSocial() throws IOException {
 		// Send message to server
 		output.println("socialTab" + "$$" + username);
@@ -302,7 +419,12 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-	// Get calorie goal from server
+	/**
+	 * getCalorieGoal
+	 * Gets calorie goal from server
+	 * @return calorie goal
+	 * @throws IOException
+	 */
 	public String getCalorieGoal() throws IOException {
 		// Send message to server
 		output.println("getCalorieGoal" + "$$" + username);
@@ -318,7 +440,19 @@ public class Client implements Runnable {
 		return (serverMessage);
 	}
 	
-    // Send meal info to server
+    /**
+     * sendMealInfo
+     * Sends meal information to server
+     * @param calories Calories
+     * @param protein Protein
+     * @param carbs Carbs
+     * @param sugar Sugar
+     * @param fiber Fiber
+     * @param fats Fats
+     * @param sodium Sodium
+     * @return Meal information
+     * @throws IOException
+     */
  	public String sendMealInfo(String calories, String protein, String carbs, String sugar, String fiber, String fats, String sodium) throws IOException {
  		// Send message to server
  		output.println("sendMealInfo" + "$$" + username + "$$" + calories + "$$" + protein + "$$" + carbs + "$$" + sugar + "$$" + fiber + "$$" + fats + "$$" + sodium);
@@ -334,7 +468,12 @@ public class Client implements Runnable {
  		return (serverMessage);
  	}
  	
-	// Get nutrtion tab info from server
+	/**
+	 * getNutritionTab
+	 * Gets nutrition tab information from server
+	 * @return nutrition tab information
+	 * @throws IOException
+	 */
   	public String getNutritionTab() throws IOException {
   		// Send message to server
   		output.println("getNutritionTab" + "$$" + username);
@@ -350,7 +489,12 @@ public class Client implements Runnable {
   		return (serverMessage);
   	}
   	
-	// Get history tab info from server
+	/**
+	 * getHistoryTab
+	 * Gets history tab information from server
+	 * @return history tab information
+	 * @throws IOException
+	 */
   	public String getHistoryTab() throws IOException {
   		// Send message to server
   		output.println("getHistoryTab" + "$$" + username);
@@ -366,7 +510,13 @@ public class Client implements Runnable {
   		return (serverMessage);
   	}
   	
-  	// Get day info from server
+  	/**
+  	 * getDayInfo
+  	 * Gets day information from server
+  	 * @param dayString Day
+  	 * @return selected day information
+  	 * @throws IOException
+  	 */
    	public String getDayInfo(String dayString) throws IOException {
    		// Send message to server
    		output.println("getDayInfo" + "$$" + username + "$$" + dayString);
@@ -382,7 +532,13 @@ public class Client implements Runnable {
    		return (serverMessage);
    	}
    	
-   	// Get workout info from a day from server
+   	/**
+   	 * getWorkoutDayInfo
+   	 * Gets workout information in current day from server
+   	 * @param dayString Day
+   	 * @return workout information in current day
+   	 * @throws IOException
+   	 */
    	public String getWorkoutDayInfo(String dayString) throws IOException {
    		// Send message to server
    		output.println("getWorkoutDayInfo" + "$$" + username + "$$" + dayString);
@@ -398,22 +554,39 @@ public class Client implements Runnable {
    		return (serverMessage);
    	}
 	
-	// Tells server to close I/O
+	/**
+	 * sendStop
+	 * Tells server to stop IO
+	 */
 	public void sendStop() {
 		output.println("stop");
 		output.flush();
 	}
 
+	/**
+	 * stop
+	 * Closes all inputs and outputs and client
+	 * @throws Exception
+	 */
 	public void stop() throws Exception {
 		input.close();
 		output.close();
 		clientSocket.close();
 	}
 	
+	/**
+	 * setUsername
+	 * Sets username
+	 * @param username Username
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * run
+	 * Run
+	 */
 	@Override
 	public void run() {
 	}
